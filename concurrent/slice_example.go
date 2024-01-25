6238dd8bdc325f63ae59e3a29dfac6dced13b10a
AlbertRyu <dgx.albert@gmail.com>
2024-01-25 15:38:47 +0800

    Update go slice append example

--- concurrent/slice_example.go
+++ concurrent/slice_example.go
@@ -4,10 +4,14 @@ var (
 	slice = make([]bool, 1)
 )
 
-func SetSlice() {
-	slice[0] = true
+func SetSlice(boolValue bool) {
+	slice[0] = boolValue
 }
 
 func GetSlice() bool {
 	return slice[0]
+}
+
+func AppendSlice(boolValue bool) {
+	slice = append(slice, boolValue)
 }
--- concurrent/slice_example_test.go
+++ concurrent/slice_example_test.go
@@ -1,23 +1,49 @@
 package concurrent
 
 import (
+	"log"
 	"math"
 	"sync"
 	"testing"
 )
 
-func TestGoSlice(t *testing.T) {
+func TestGoSliceAppend(t *testing.T) {
 	wg := new(sync.WaitGroup)
-	for i := 0; i < 1000; i++ {
+	for i := 0; i < 5000; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
 			if math.Mod(float64(i), 2) == 0 {
-				SetSlice()
+				AppendSlice(true)
 			} else {
-				GetSlice()
+				AppendSlice(false)
 			}
 		}()
 	}
 	wg.Wait()
+
+	log.Println(len(slice))
+}
+
+func TestGoSliceAppendWithLock(t *testing.T) {
+	wg := new(sync.WaitGroup)
+	mux := new(sync.Mutex)
+	for i := 0; i < 5000; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			if math.Mod(float64(i), 2) == 0 {
+				mux.Lock()
+				AppendSlice(true)
+				mux.Unlock()
+			} else {
+				mux.Lock()
+				AppendSlice(false)
+				mux.Unlock()
+			}
+		}()
+	}
+	wg.Wait()
+
+	log.Println(len(slice))
 }
